67f73f95f2af494b995a9783e6cfec376f201fe1
honey the codewitch <gaztoast@gmail.com>
2022-12-04 02:05:31 -0800

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,19 @@
 using namespace arduino;
 using namespace gfx;
 
-using bus_t = tft_spi_ex<LCD_HOST, PIN_NUM_CS, PIN_NUM_MOSI, PIN_NUM_MISO, PIN_NUM_CLK, SPI_MODE0,true,LCD_WIDTH*LCD_HEIGHT*2,2>;
+using bus_t = tft_spi_ex<LCD_HOST, PIN_NUM_CS, PIN_NUM_MOSI, PIN_NUM_MISO, PIN_NUM_CLK, SPI_MODE0,true,LCD_WIDTH*LCD_HEIGHT*2+8,2>;
 using display_t = st7789<LCD_WIDTH, LCD_HEIGHT, PIN_NUM_DC, PIN_NUM_RST, PIN_NUM_BCKL, bus_t, 0, true, 400, 200>;
 using color_t = color<typename display_t::pixel_type>;
 
 using button_1_t = button<35, 10, true>;
 using button_2_t = button<0, 10, true>;
-using rgb_t = rgb_pixel<24>;
 
 static display_t dsp;
 
 button_1_t button_1;
 button_2_t button_2;
+
+// SNIP ---- delete after the above line for new projects.
 
 const int16_t
     res_bits = 12,                        // Fractional resolution
